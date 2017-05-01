3bf4bf2d3c6fd557205e625a23bd023c9234c2da
Sasha Hart <harts@boundlessgeo.com>
2017-05-01 10:30:50 -0500

    add material-ui dependency to match sdk examples

--- example/src/index.js
+++ example/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import App from './app';
 import './index.css';
 
+injectTapEventPlugin();
 ReactDOM.render(
   <App />,
   document.getElementById('root')
