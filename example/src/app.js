8457fa35419e43f1ba3092a25f8b577170d1c920
Sasha Hart <harts@boundlessgeo.com>
2017-05-01 10:29:36 -0500

    use single quotes in example

--- example/src/app.js
+++ example/src/app.js
@@ -1,25 +1,25 @@
-import { Because } from "because";
-import React, { Component } from "react";
-import "./app.css";
+import { Because } from 'because';
+import React, { Component } from 'react';
+import './app.css';
 
-import Login from "./login";
-import Routing from "./routing";
-import Geocoding from "./geocoding";
-import ReverseGeocoding from "./reverse_geocoding";
-import Search from "./search";
+import Login from './login';
+import Routing from './routing';
+import Geocoding from './geocoding';
+import ReverseGeocoding from './reverse_geocoding';
+import Search from './search';
 
 
 class App extends Component {
   constructor (props) {
         super(props);
         this.state = {
-            bcs: new Because("test", true)  // env="test", debug=true
+            bcs: new Because('test', true)  // env='test', debug=true
         };
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
       <h1>because.js demo</h1>
 
         <h2>Login</h2>
