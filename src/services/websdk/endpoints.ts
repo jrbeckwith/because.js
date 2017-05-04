b504913b3ee3223f875ff850499e4501ac3d551e
Sasha Hart <harts@boundlessgeo.com>
2017-05-04 16:17:36 -0500

    note websdk endpoints

--- src/services/websdk/endpoints.ts
+++ src/services/websdk/endpoints.ts
@@ -2,6 +2,24 @@ import { Endpoint } from "../../service";
 
 
 export const endpoints = {
+
+    // bcs/bcs-websdk-service/src/main/java/com/boundlessgeo/bcs
+    // /websdk/controller/WebSdkPackageController.java
+
+    // getWebsdkGeneratorVerison
+    "version": new Endpoint(
+        "GET", "/websdk/version",
+    ),
+
+    // buildAndPackageProject
+    "package": new Endpoint(
+        // Does not support GET. has @PostMapping
+        // needs a file
+        // returns ???
+        // ultimately application/zip
+        "POST", "/websdk/package",
+    ),
+
     "manage": new Endpoint(
         "GET", "/websdk/manage",
     ),
